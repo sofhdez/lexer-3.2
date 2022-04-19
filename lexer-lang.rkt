798a0be83c1ce52b67f6db0f17c39f2ec7002b4d
Sofia Hernandez <sofia.hdezm1@gmail.com>
2022-04-19 14:04:51 -0500

    cambio de nombre del lexer

--- lexer-lang.rkt
+++ lexer-lang.rkt
@@ -10,75 +10,75 @@
 
 (require (prefix-in : parser-tools/lex-sre))
 
-(define calc-lexer
+(define lang-lexer
   (lexer
    [(:+ (:or (char-range #\a #\z) (char-range #\A #\Z)))
 
     ; ========> Variable
 
     (cons `(Variable ,(string->symbol lexeme))
-          (calc-lexer input-port))]
+          (lang-lexer input-port))]
 
    ; ========> Comentarios
    [(:: "//" (complement (:: any-string "//" any-string)))  ; falta que reconozca lo que sigue ed //
 
     (cons `(Comentario ,(string->symbol lexeme))
-          (calc-lexer input-port))]
+          (lang-lexer input-port))]
 
    ;; ========> Símbolos especiales
    [#\(
     ; => Paréntesis que abre
     (cons '(Paréntesis que abre)
-          (calc-lexer input-port))]
+          (lang-lexer input-port))]
 
    [#\)
     ; => Paréntesis que cierra
     (cons '(Paréntesis que cierra)
-          (calc-lexer input-port))]
+          (lang-lexer input-port))]
 
    [(:: (:? #\-) (:+ (char-range #\0 #\9)))
     ; => Enteros
     (cons `(INT ,(string->number lexeme))
-          (calc-lexer input-port))]
+          (lang-lexer input-port))]
 
    ;; ========> Operadores
    [#\=
     ; => Asignación
     (cons `(Asignación ,(string->symbol lexeme))
-          (calc-lexer input-port))]
+          (lang-lexer input-port))]
 
    [#\+
     ; => Suma
     (cons `(Suma ,(string->symbol lexeme))
-          (calc-lexer input-port))]
+          (lang-lexer input-port))]
 
    [#\-
     ; => Resta
     (cons `(Resta ,(string->symbol lexeme))
-          (calc-lexer input-port))]
+          (lang-lexer input-port))]
 
    [#\*
     ; => Multiplicación
     (cons `(Multiplicación ,(string->symbol lexeme))
-          (calc-lexer input-port))]
+          (lang-lexer input-port))]
 
    [#\/
     ; => División
     (cons `(División ,(string->symbol lexeme))
-          (calc-lexer input-port))]
+          (lang-lexer input-port))]
 
    [#\^
     ; => Potencia
     (cons `(Potencia ,(string->symbol lexeme))
-          (calc-lexer input-port))]
+          (lang-lexer input-port))]
 
    [whitespace
     ; =>
-    (calc-lexer input-port)]
+    (lang-lexer input-port)]
 
    [(eof)
     '()]
    ))
 
 ;; archivo muchas líneas
-(calc-lexer (open-input-file "micodigo.txt"))
+(lang-lexer (open-input-file "micodigo.txt"))
