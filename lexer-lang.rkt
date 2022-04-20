9cb01b0eb8679f5a1903080cf7e7edef5c40150c
Sofia Hernandez <sofia.hdezm1@gmail.com>
2022-04-19 20:28:56 -0500

    lista de lo que falta

--- lexer-lang.rkt
+++ lexer-lang.rkt
@@ -1,8 +1,14 @@
 #lang slideshow
-; main
-; Faltan
+
+; TODO
 ;     - Flotantes (reales) solo los negativos
-;     - Regla de incio de las variables
+;     - Regla de las variables
+;           Variables:
+;                 Deben empezar con una letra (mayúscula o minúscula).
+;                 Sólo están formadas por letras, números y underscore (‘_’).
+;     - Nomenclatura del archivo output
+;           Lo mismo debe ser guardado en un archivo con la nomenclatura: 
+;           nombreDelArchivoDeEntrada+"-output.txt"
 
 (require racket/generator)
 (require parser-tools/lex)
@@ -11,8 +17,7 @@
 
 (require (prefix-in : parser-tools/lex-sre))
 
-; Función que crea el output.txt
-(define file (open-output-file "output.txt"))
+; -------------- Función que crea el output.txt --------------
 (define (generate file lst)
   (if(not(null? lst))
      (begin
@@ -23,37 +28,38 @@
        (list)))
   (close-output-port file))
 
-(define lang-lexer
+; -------------------------- Lexer --------------------------
+(define lexerAritmetico
   (lexer
    [(:+ (:or (char-range #\a #\z) (char-range #\A #\Z)))
 
     ; ========> Variable
 
     (cons `(Variable ,(string->symbol lexeme))
-          (lang-lexer input-port))]
+          (lexerAritmetico input-port))]
 
    ; ========> Comentarios
    [(:: "//" (complement (:: any-string "//" any-string)))  ; falta que reconozca lo que sigue ed //
 
     (cons `(Comentario ,(string->symbol lexeme))
-          (lang-lexer input-port))]
+          (lexerAritmetico input-port))]
 
    ;; ========> Símbolos especiales
    [#\(
     ; => Paréntesis que abre
     (cons '(Paréntesis que abre)
-          (lang-lexer input-port))]
+          (lexerAritmetico input-port))]
 
    [#\)
     ; => Paréntesis que cierra
     (cons '(Paréntesis que cierra)
-          (lang-lexer input-port))]
+          (lexerAritmetico input-port))]
 
    ;; ========> Números
    [(:: (:? #\-) (:+ (char-range #\0 #\9)))
     ; => Enteros
     (cons `(Entero ,(string->number lexeme))
-          (lang-lexer input-port))]
+          (lexerAritmetico input-port))]
 
    [
     (:or (:or (:: (:? (:+ (char-range #\0 #\9))) 
@@ -74,48 +80,50 @@
     ;     (exponent (:: (:or "e" "E") (:? (:or "+" "-")) (:+ (char-range #\0 #\9))))
     ; => Flotantes (reales)
     (cons `(Real ,(string->number lexeme))
-          (lang-lexer input-port))]
+          (lexerAritmetico input-port))]
 
    ;; ========> Operadores
    [#\=
     ; => Asignación
     (cons `(Asignación ,(string->symbol lexeme))
-          (lang-lexer input-port))]
+          (lexerAritmetico input-port))]
 
    [#\+
     ; => Suma
     (cons `(Suma ,(string->symbol lexeme))
-          (lang-lexer input-port))]
+          (lexerAritmetico input-port))]
 
    [#\-
     ; => Resta
     (cons `(Resta ,(string->symbol lexeme))
-          (lang-lexer input-port))]
+          (lexerAritmetico input-port))]
 
    [#\*
     ; => Multiplicación
     (cons `(Multiplicación ,(string->symbol lexeme))
-          (lang-lexer input-port))]
+          (lexerAritmetico input-port))]
 
    [#\/
     ; => División
     (cons `(División ,(string->symbol lexeme))
-          (lang-lexer input-port))]
+          (lexerAritmetico input-port))]
 
    [#\^
     ; => Potencia
     (cons `(Potencia ,(string->symbol lexeme))
-          (lang-lexer input-port))]
+          (lexerAritmetico input-port))]
 
    [whitespace
     ; =>
-    (lang-lexer input-port)]
+    (lexerAritmetico input-port)]
 
    [(eof)
     '()]
    ))
 
 ;; archivo muchas líneas
-(lang-lexer (open-input-file "micodigo.txt"))
+(define input (open-output-file "output.txt"))
 
-(generate file (lang-lexer (open-input-file "micodigo.txt")))
+(lexerAritmetico (open-input-file "micodigo.txt"))
+
+(generate input (lexerAritmetico (open-input-file "micodigo.txt")))
