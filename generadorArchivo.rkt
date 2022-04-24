88bedb44363d78f4c35143f31de4c0880f41b499
Sofia Hernandez <sofia.hdezm1@gmail.com>
2022-04-24 14:10:52 -0500

    nombres del equipo

--- generadorArchivo.rkt
+++ generadorArchivo.rkt
@@ -1,3 +1,9 @@
+; Actividad 3.2 Programando parte de un Lenguaje de Programación
+; Sofía Margarita Hernández Muñoz A01655084
+; Emiliano Saucedo Arriola A01659258
+; Alfonso Pineda Castillo A01660394
+; Gael Eduardo Pérez Gómez A01753336
+
 #lang slideshow
 
 ; -------------- Función que crea el output.txt --------------
